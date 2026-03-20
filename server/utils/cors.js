7728208ea3c0201817c86273552a699733bc9101
Satyam Singh <1ssatyamsinghs@gmail.com>
2026-03-20 13:46:51 +0530

    fix: update CORS configuration to allow requests from production URL

--- server/utils/cors.js
+++ server/utils/cors.js
@@ -1,5 +1,5 @@
 export function applyCors(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "https://lms-nhwv.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
